b5e9643a4be538dfb38da298eb57ae9707847dd0
D3n3tsu <ddm_09@ukr.net>
2017-04-14 19:46:08 +0300

    Model created.

---
+++ MVCToolsTutorial/Models/LinqValueCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCToolsTutorial.Models
+{
+    public class LinqValueCalculator
+    {
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return products.Sum<Product>(p => p.Price);
+        }
+    }
+}
